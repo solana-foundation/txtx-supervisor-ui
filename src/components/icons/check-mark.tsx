b42583340f167af3827a4e989eec07258db1b24e
MicaiahReid <micaiahreid@gmail.com>
2024-04-21 22:55:03 -0400

    add check mark

---
+++ src/components/icons/check-mark.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function CheckMark() {
+  return (
+    <svg
+      width="25"
+      height="24"
+      viewBox="0 0 25 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g id="Icon/Action/check">
+        <path
+          id="Vector"
+          d="M9.57776 15.642L19.2198 6L20.5 7.28025L9.57776 18.2025L4.49951 13.1257L5.77976 11.8455L9.57776 15.642Z"
+          fill="white"
+        />
+      </g>
+    </svg>
+  );
+}
