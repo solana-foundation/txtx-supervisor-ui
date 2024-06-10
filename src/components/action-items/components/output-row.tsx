7a2d27b1067f2ea33f874c2291db84bbf196ff71
MicaiahReid <micaiahreid@gmail.com>
2024-06-10 17:23:02 -0400

    some formatting

--- src/components/action-items/components/output-row.tsx
+++ src/components/action-items/components/output-row.tsx
@@ -6,7 +6,7 @@ export interface OutputRow {
 export function OutputRow({ displayValue }: OutputRow) {
   return (
     <div className="overflow-auto scrollbar-thin self-stretch flex-col justify-start items-start flex">
-      <div className="w-fit self-stretch bg-black rounded flex-col justify-start items-start flex">
+      <div className="w-fit min-w-full self-stretch bg-black rounded flex-col justify-start items-start flex">
         <div className="w-full p-4 bg-black bg-opacity-0 justify-start items-start inline-flex">
           <div className="grow shrink basis-0 text-white text-sm font-medium font-inter">
             {displayValue}
