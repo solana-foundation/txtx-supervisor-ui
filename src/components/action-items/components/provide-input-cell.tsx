776445f0cc710510c2a3c965532574feba573f87
yakus <eug.heartbeat@gmail.com>
2024-07-23 13:42:29 +0300

    add Pencil icon

--- src/components/action-items/components/provide-input-cell.tsx
+++ src/components/action-items/components/provide-input-cell.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { ActionItemRequest, DisplayableValue } from "../../main/types";
 import { classNames } from "../../../utils/helpers";
 import { XMarkIcon } from "@heroicons/react/20/solid";
+import Pencil from "../../icons/pencil";
 
 export interface ProvideInputCell {
   actionItem: ActionItemRequest;
@@ -47,38 +48,11 @@ export function ProvideInputCell({
             onChange={debouncedOnChange}
           />
           <div className="w-[32px] bg-neutral-800 flex items-center justify-center rounded-r-sm border-gray-800 border border-l-0">
-            <svg
-              width="12"
-              height="12"
-              viewBox="0 0 12 12"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
+            <Pencil
               className={
                 status === "Success" ? "text-emerald-500" : "text-gray-400"
               }
-            >
-              <path
-                d="M10.5332 2.71012L9.29035 1.46723C9.06215 1.239 8.69002 1.23885 8.46201 1.46688L2.125 7.80389L4.19658 9.87549L10.5336 3.53848C10.7616 3.31045 10.7615 2.93834 10.5332 2.71012Z"
-                stroke="currentColor"
-                stroke-miterlimit="22.926"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                d="M1.29688 10.7041L2.12553 7.80387L4.19711 9.87545L1.29688 10.7041Z"
-                stroke="currentColor"
-                stroke-miterlimit="22.926"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                d="M9.70441 4.36743L7.63281 2.29583"
-                stroke="currentColor"
-                stroke-miterlimit="22.926"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg>
+            />
           </div>
         </div>
       </div>
