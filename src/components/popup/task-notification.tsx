30459ac011d524a24b5434f7ac2d996d8414e48e
mike <mjoecohen@gmail.com>
2024-09-13 10:52:20 +0100

    Uses a new component (TaskNotification) to hook onto to the progress bar events dispatched by useSubscription in response to graphql responses.

--- src/components/popup/task-notification.tsx
+++ src/components/popup/task-notification.tsx
@@ -1,94 +1,86 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import usePageVisibility from '../../hooks/usePageVisibility'; // Same hook from before
+import { ProgressBarStatus, ProgressBlock } from '../main/types';
+import { selectVisibleProgressBlock } from '../../reducers/runbooks-slice';
+import { useSelector } from 'react-redux';
 
-const changeFavicon = (faviconURL) => {
+const TITLE_TEXT = ['txtx runbooks', 'txtx - task in progress...', 'txtx - task complete'];
+const TITLE_ICONS = ['favicon', 'red_light', 'green_light'];
+
+const changeTab = (taskStatus:number) => {
     const link = document.querySelector("link[rel*='icon']") || document.createElement('link') as any;
     link.type = 'image/x-icon';
     link.rel = '[rel*="icon"]';
-    link.href = `${faviconURL}?v=${new Date().getTime()}`;
+    link.href = `/assets/${TITLE_ICONS[taskStatus]}.png?v=${new Date().getTime()}`;
     document.getElementsByTagName('head')[0].appendChild(link);
-};
+    document.title = TITLE_TEXT[taskStatus];
+  };
 const requestNotificationPermission = () => {
     if (Notification.permission !== 'granted') {
       Notification.requestPermission();
     }
-  };
+};
   
-  const showNotification = (title, options) => {
+const showNotification = (title, options) => {
     if (Notification.permission === 'granted') {
       new Notification(title, options);
     }
 };
 
 const TaskNotification = () => {
-    const [taskCompleted, setTaskCompleted] = useState(false);
-    const isHidden = usePageVisibility(); // This will return true if the tab is hidden
-
-    const defaultFavicon = '/assets/red_light.png';
-    const completedFavicon = '/assets/green_light.png';
-    const inProgressFavicon = '/assets/red_light.png';
-
-    let taskTimeout;
-    function startTimeoutTask() {
-        taskTimeout = setTimeout(() => {
-            setTaskCompleted(true);
-        }, 15000);
-    }
-
-    function clearTimeoutTask() {
-        clearTimeout(taskTimeout);
-    }
+  const isHidden = usePageVisibility();
+  const [taskStatus, setTaskStatus] = useState(0);
+  const progressBlocks = useSelector(selectVisibleProgressBlock);
+  const pushProgressBlockStatus = useSelector(selectVisibleProgressBlock);
 
     useEffect(() => {
-        // Request notification permission when component mounts
-        requestNotificationPermission();
-    
-        // Simulate a task completion after 5 seconds
-        const taskTimeout = setTimeout(() => {
-          setTaskCompleted(true);
-        }, 5000);
-    
-        return () => {
-          clearTimeout(taskTimeout);
-        };
-      }, []);
-    
-      useEffect(() => {
-        if (taskCompleted) { // && !isVisible) {
-          // Show notification if task is completed and user is not on the current tab
+      if (taskStatus === 2) {
+        if (isHidden) {
           showNotification('Task Complete', {
             body: 'Your task has been completed successfully!',
-            icon: '/path/to/icon.png',
+            icon: '/assets/favicon.png',
           });
         }
-      }, [taskCompleted, isHidden]);
+      }
+      changeTab(taskStatus);
+      setTimeout(() => {
+        changeTab(0);
+      }, 5000);
+  
+      return () => {
+        changeTab(0);
+      };
+    }, [isHidden, taskStatus]);
 
-      useEffect(() => {
-        if (isHidden) {
-            if (taskCompleted) {
-                document.title = 'task complete!';
-                changeFavicon(completedFavicon);
-            } else {
-                document.title = 'task in progress...';
-                changeFavicon(inProgressFavicon);
-            }
-            startTimeoutTask()
-        } else {
-            document.title = 'txtx runbooks!';
-            changeFavicon(defaultFavicon);
-            clearTimeoutTask();
-            setTaskCompleted(false);
+    useEffect(() => {
+      if (progressBlocks !== undefined) {
+        const block: ProgressBlock = progressBlocks;
+        console.log('TaskNotification:progressBlockEvent: ', progressBlocks)
+        if (block && block.type === 'ProgressBar' && block.visible && block.panel.length === 0) {
+          requestNotificationPermission();
+          setTaskStatus(1);
+          console.log('TaskNotification:progressBlockEvent: setTaskStatus(1)')
+      }
+      }
+    }, [progressBlocks]);
+
+    useEffect(() => {
+      if (pushProgressBlockStatus?.type === 'ProgressBar') {
+        console.log('TaskNotification:pushProgressBlockStatus: ', pushProgressBlockStatus)
+        const statuses:Array<ProgressBarStatus> = pushProgressBlockStatus?.panel[0]?.statuses || []
+        // the action holds an array of the all the progress updates - the complete looks to be the last.
+        const idx = statuses.findIndex((o) => o.status.toLowerCase().startsWith('complete'))
+        if (idx > -1) {
+          setTaskStatus(2);
+          console.log('TaskNotification:progressBlockEvent: setTaskStatus(2)')
         }
-
-        return () => {
-            document.title = 'txtx runbooks'; // Reset title when component unmounts
-        };
-    }, [isHidden, taskCompleted]);
-
+      }
+    }, [pushProgressBlockStatus]);
+    
     return (
         <div>
             <h1>Task Status</h1>
-            {taskCompleted ? <p>Task Completed!</p> : <p>Task in progress...</p>}
+            {taskStatus ? <p>Task Completed!</p> : <p>Task in progress...</p>}
         </div>
       );
 };
